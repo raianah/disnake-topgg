29ed9d5ddd5ae0f62a8df046b0077f71891485c7
rai <82108512+raianah@users.noreply.github.com>
2022-08-04 18:10:48 +0800

    added files

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. topggpy documentation master file, created by
+   sphinx-quickstart on Thu Feb  8 18:32:44 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+###################################
+Welcome to topggpy's documentation!
+###################################
+
+.. toctree::
+   :maxdepth: 2
+
+   api
+   models
+   webhooks
+   exceptions
+   whats_new
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ tests/test_webhook.py
@@ -0,0 +1,68 @@
+from typing import TYPE_CHECKING, Dict
+
+import aiohttp
+import pytest
+from disnake import Client
+
+from topgg import WebhookManager
+
+if TYPE_CHECKING:
+    from topgg.types import BotVoteData, ServerVoteData
+
+auth = "youshallnotpass"
+
+
+@pytest.fixture
+def webhook_manager() -> WebhookManager:
+    return WebhookManager(Client()).dbl_webhook("/dbl", auth).dsl_webhook("/dsl", auth)
+
+
+def test_WebhookManager_routes(webhook_manager: WebhookManager) -> None:
+    assert len(webhook_manager._webhooks) == 2
+
+
+@pytest.mark.asyncio
+async def test_WebhookManager_run_method(webhook_manager: WebhookManager) -> None:
+    task = webhook_manager.run(5000)
+
+    try:
+        if not task.done():
+            assert await task is None
+
+        assert hasattr(webhook_manager, "_webserver")
+    finally:
+        await webhook_manager.close()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "headers, result, state",
+    [({"authorization": auth}, 200, True), ({}, 401, False)],
+)
+async def test_WebhookManager_validates_auth(
+    webhook_manager: WebhookManager, headers: Dict[str, str], result: int, state: bool
+) -> None:
+    await webhook_manager.run(5000)
+
+    dbl_state = dsl_state = False
+
+    @webhook_manager.bot.event
+    async def on_dbl_vote(data: "BotVoteData") -> None:
+        nonlocal dbl_state
+        dbl_state = True
+
+    @webhook_manager.bot.event
+    async def on_dsl_vote(data: "ServerVoteData") -> None:
+        nonlocal dsl_state
+        dsl_state = True
+
+    try:
+        for path in ("dbl", "dsl"):
+            async with aiohttp.request(
+                "POST", f"http://localhost:5000/{path}", headers=headers, json={}
+            ) as r:
+                assert r.status == result
+
+            assert locals()[f"{path}_state"] is state
+    finally:
+        await webhook_manager.close()
